c5b092d008e208c60ca8136cf21cf7f0a2fcc651
physerikc <86751702+physerikc@users.noreply.github.com>
2023-11-05 18:18:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+const buttons = document.querySelectorAll(".button");
+const characters = document.querySelectorAll(".character")
+
+buttons.forEach((button, index) => {
+    button.addEventListener("click", () => {
+        
+        const buttonSelected = document.querySelector(".button.selected");
+        buttonSelected.classList.remove("selected");
+        button.classList.add("selected");
+
+
+
+        const characterSelected = document.querySelector(".character.selected");
+        characterSelected.classList.remove("selected");
+
+        characters[index].classList.add("selected");
+
+    });
+});
